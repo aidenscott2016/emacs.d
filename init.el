47543d2113eeddeabe4cb8afafe22e4380b3ea72
Aiden <aiden@oldstreetjournal.co.uk>
2024-01-19 10:34:34 +0000

    change ivy keymaps

--- init.el
+++ init.el
@@ -40,15 +40,16 @@
   :diminish
   :bind (("C-s" . swiper)
          :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
          ("C-l" . ivy-alt-done)
          ("C-j" . ivy-next-line)
          ("C-k" . ivy-previous-line)
          :map ivy-switch-buffer-map
+         ("C-j" . ivy-next-line)
          ("C-k" . ivy-previous-line)
          ("C-l" . ivy-done)
          ("C-d" . ivy-switch-buffer-kill)
          :map ivy-reverse-i-search-map
+         ("C-j" . ivy-next-line)
          ("C-k" . ivy-previous-line)
          ("C-d" . ivy-reverse-i-search-kill))
   :config
